95de95197f1bf54e7da303b39798a5481fa4ec6e
Dmytro Klymenko <dklymenko.work@gmail.com>
2024-09-10 21:09:20 +0200

    add config files from another repo as base

---
+++ packages/wdio-driver-manager/src/@types/vite.d.ts
@@ -0,0 +1,7 @@
+declare module '@wdio/mocha-framework/common'
+
+declare module 'virtual:wdio' {
+    export const commands: string[]
+    export const automationProtocolPath: string
+    export const matchers: string[]
+}
