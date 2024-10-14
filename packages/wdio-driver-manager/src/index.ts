e47496af052fe853a870cd2ced8d1e8c84d5d96e
Dmytro Klymenko <dklymenko.work@gmail.com>
2024-10-14 19:39:58 +0200

    refactoring: split driver-manager class to 2 separate classes - Client and Server; add methods to Client class

--- packages/wdio-driver-manager/src/index.ts
+++ packages/wdio-driver-manager/src/index.ts
@@ -1,4 +1,44 @@
-export default class DriverManager {
+import { startWebDriver } from './startWebDriver.js'
+import type { Capabilities } from '@wdio/types'
+
+// import WorkerInstance from '@wdio/local-runner'
+
+const binaryDriversPool = new Map()
+
+export class DriverManagerClient {
+
+    constructor() {
+
+    }
+    // TODO-my: probably need to remove
+    // createWorkerInstance(config: any, workerOptions: any, stdout: any, stderr: any) {
+        // return new WorkerInstance(config, workerOptions, stdout, stderr)
+    // }
+
+    /**
+     *
+     * Start binary process and add it to pool, linking it with worker process by workerUuid
+     */
+    async startProcess(workerUuid: string, options: Capabilities.RemoteConfig) {
+        const binaryDriverProcess = await startWebDriver(options)
+        binaryDriversPool.set(workerUuid, binaryDriverProcess)
+    }
+
+    stopProcess(workerUuid: string) {
+        throw new Error('Not implemented')
+    }
+
+    /**
+     *
+     * Get process binary id for linked worker process from pool by workerUuid
+     */
+    getProcessId(workerUuid: string) {
+        return binaryDriversPool.get(workerUuid).pid
+    }
+
+}
+
+export class DriverManagerServer {
     constructor() {
 
     }
