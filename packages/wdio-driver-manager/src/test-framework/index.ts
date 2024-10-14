98ff6fdcdc763407fb691303c267e0d048de5f1a
Dmytro Klymenko <dklymenko.work@gmail.com>
2024-10-14 19:28:47 +0200

    refactoring: add code to driver-manager from Utils

---
+++ packages/wdio-driver-manager/src/test-framework/index.ts
@@ -0,0 +1,6 @@
+/* istanbul ignore file */
+
+import { testFnWrapper } from './testFnWrapper.js'
+import { wrapGlobalTestMethod } from './testInterfaceWrapper.js'
+
+export { testFnWrapper, wrapGlobalTestMethod }
